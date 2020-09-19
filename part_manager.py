83cce85ff87f1d71484d183464d96e0dd9f49b08
Mahmoud <mahmoudhadbeh@gmail.com>
2020-09-18 20:33:37 -0400

    first version

---
+++ part_manager.py
@@ -0,0 +1,23 @@
+# GUI Part Manager from YouTube Tutorial 
+# Red Alert Technologies, LLC
+
+from tkinter import *
+
+
+
+#App define / Title 
+app = Tk()
+app.title("Part Manager")
+app.geometry("700x350")
+
+# Part
+part_text = StringVar()
+part_label = Label(app, text="Part Name", font=('bold', 14), pady=20)
+part_label.grid(row=0, column=0, sticky=W)
+part_entry = Entry(app, textevariable = part_text)
+part_entry.grid(row=0, column=1)
+
+
+
+# Main Loop to Start Program 
+app.mainloop()
